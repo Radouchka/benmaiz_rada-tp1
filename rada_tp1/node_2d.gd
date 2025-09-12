43766f2d8afa4672d0ab7d2ecee75a2255247ee1
radouchka <meow@gmail.com>
2025-09-12 14:58:19 -0400

    innit 6

--- rada_tp1/node_2d.gd
+++ rada_tp1/node_2d.gd
@@ -6,3 +6,15 @@ func _on_orange_cat_pressed() -> void:
 	sprite.stop()
 	sprite.play("orangee")
 	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("grey_kitty"):
+		if $Control/grey_cat/CharacterBody2D/AnimatedSprite2D.is_playing():
+			$Control/grey_cat/CharacterBody2D/AnimatedSprite2D.stop()
+		else:
+			$Control/grey_cat/CharacterBody2D/AnimatedSprite2D.play("meowing")
+			
+	if event.is_action_pressed("orange_kitty"):
+		if $Control/orange_cat/CharacterBody2D/AnimatedSprite2Dd.is_playing():
+			$Control/orange_cat/CharacterBody2D/AnimatedSprite2Dd.stop()
+		else:
+			$Control/orange_cat/CharacterBody2D/AnimatedSprite2Dd.play("orangee")
